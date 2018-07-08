2b8c3b0d01116fa68f2aa0e2b9fcfa801adcd38c
csieradzki <dev@christophe-sieradzki.fr>
2018-07-08 16:39:57 +0200

    feat(config): create config

---
+++ System/Core/Config.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Hexacore\Core;
+
+class Config {
+
+  private $params;
+  private static $instance;
+
+  // Renvoie la valeur d'un paramètre de configuration
+
+  public static function get(string $filepath) : Config
+  {
+    if (is_null(self::$instance)) {
+      self::$instance = new Config($filepath);
+    }
+
+    return self::$instance;
+  }
+
+  private function __contructor(string $filepath = __DIR__ . "/../../App/config/prod.json") : void
+  {
+    $string = file_get_contents($filepath);
+    $this->params = json_decode($string, true);
+  }
+}
