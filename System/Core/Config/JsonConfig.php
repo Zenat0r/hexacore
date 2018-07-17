b8009b787727756b431db48d8e3efd299151e4b7
csieradzki <dev@christophe-sieradzki.fr>
2018-07-18 00:03:54 +0200

    docs(config): add function description

--- System/Core/Config/JsonConfig.php
+++ System/Core/Config/JsonConfig.php
@@ -4,9 +4,15 @@ namespace Hexacore\Core\Config;
 
 class JsonConfig implements ConfigInterface
 {
+    /** @var array */
     private $params;
+    
+    /** @var JsonConfig */
     private static $instance;
 
+    /**
+     *  {@inheritDoc}
+     */
     public static function get(string $filepath = __DIR__ . "/../../App/config/app.json") : array
     {
         if (is_null(self::$instance)) {
@@ -22,7 +28,7 @@ class JsonConfig implements ConfigInterface
         $this->params = json_decode($string, true);
     }
 
-    private function getParams()
+    private function getParams(): array
     {
         return $this->params;
     }
