b76e0d9077e097d16e4078ed3bb1bf161d6874c0
csieradzki <dev@christophe-sieradzki.fr>
2018-07-21 17:58:37 +0200

    feat(config): can now handle several config file

--- System/Core/Config/JsonConfig.php
+++ System/Core/Config/JsonConfig.php
@@ -15,15 +15,22 @@ class JsonConfig implements ConfigInterface
      */
     public static function get(string $name = "system") : array
     {
-        $filepath = "../../../App/config/" . $name . ".json";
-        if (is_null(self::$instance)) {
+        $filepath = __DIR__ . "/../../../App/config/" . $name . ".json";
+        if (self::$instance === null) {
             self::$instance = new JsonConfig($filepath);
+        } elseif (empty($this->params[$filepath])) {
+            $this->setParm($filepath);
         }
 
-        return (self::$instance)->getParam($filepath);
+        return self::$instance->getParam($filepath);
     }
 
-    private function __contructor(string $filepath) : void
+    private function __construct(string $filepath)
+    {
+        $this->setParm($filepath);
+    }
+
+    private function setParm(string $filepath) : void
     {
         $string = file_get_contents($filepath);
         $this->params[$filepath] = json_decode($string, true);
