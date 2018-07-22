879a4e14b02e21f3e5910020d99b87a34669c99c
csieradzki <dev@christophe-sieradzki.fr>
2018-07-22 16:21:28 +0200

    docs(firewall): fonction documentation

--- System/Core/Firewall/FirewallInterface.php
+++ System/Core/Firewall/FirewallInterface.php
@@ -6,5 +6,12 @@ use Hexacore\Core\Request\RequestInterface;
 
 interface FirewallInterface
 {
+    /**
+     * Check if the request header are conform to the application configuration
+     *
+     * @param RequestInterface $request
+     * @param boolean $throw
+     * @return boolean
+     */
     public function check(RequestInterface $request, bool $throw = true) : bool;
 }
