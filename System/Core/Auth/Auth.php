c4f89c533b92bf54c9402df584b47acfa589b2a0
csieradzki <dev@christophe-sieradzki.fr>
2018-07-22 16:21:07 +0200

    feat(auth): created

---
+++ System/Core/Auth/Auth.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Hexacore\Core\Auth;
+
+use Hexacore\Core\Storage\StorageInterface;
+use Hexacore\Core\Config\JsonConfig;
+
+class Auth implements AuthInterface
+{
+    const defaultRole = "ANONYMOUS";
+
+    private $roles;
+
+    public function __construct()
+    {
+        $this->roles = JsonConfig::get("app")["Auth"]["roles"] ?? [];
+        array_push($this->roles, Auth::defaultRole);
+    }
+
+    public function isGranted(StorageInterface $storage, string $role) : bool
+    {
+        $role = strtoupper($role);
+        if (!in_array($role, $this->roles)) {
+            //throw role doesn't exist
+        }
+
+        if ($storage->get("USER_ROLE") === $role) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getToken(StorageInterface $storage) : ?string
+    {
+        $token = $storage->get("token") ?? null;
+
+        return $token;
+    }
+
+    public function authenticate(StorageInterface $storage) : void
+    {
+        $token = $this->getToken($storage);
+
+        if (null === $token) {
+            $token = $storage->add("token", md5(uniqid()));
+        }
+
+        $storage->add("USER_ROLE", Auth::defaultRole);
+    }
+}
---
+++ System/Core/Auth/AuthInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Hexacore\Core\Auth;
+
+use Hexacore\Core\Storage\StorageInterface;
+
+interface AuthInterface
+{
+    /**
+     * Check is a user has the right depentin on a role
+     *
+     * @param StorageInterface $storage
+     * @param string $role
+     * @return boolean
+     */
+    public function isGranted(StorageInterface $storage, string $role) : bool;
+
+    /**
+     * Return the Auth unique itentificator
+     *
+     * @param StorageInterface $storage
+     * @return string|null
+     */
+    public function getToken(StorageInterface $storage) : ?string;
+
+    /**
+     * Authenticate the client
+     *
+     * @param StorageInterface $storage
+     * @return void
+     */
+    public function authenticate(StorageInterface $storage) : void;
+}
