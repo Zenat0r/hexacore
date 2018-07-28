3aaa6e1aebc90dedbfd100bdadd390673c5d5036
csieradzki <dev@christophe-sieradzki.fr>
2018-07-28 17:42:39 +0200

    feat(controller): created abstract controller

--- App/src/Controller/IndexController.php
+++ App/src/Controller/IndexController.php
@@ -2,51 +2,57 @@
 
 namespace App\Controller;
 
-class indexController{
+use Hexacore\Core\Storage\Session\SessionInterface;
+use Hexacore\Core\Auth\AuthInterface;
+use Hexacore\Core\Controller;
+use Hexacore\Core\Firewall\DefaultFirewall;
 
-  public function __construct() {
-    
-  }
+class indexController extends Controller
+{
+    public function __construct(SessionInterface $session)
+    {
+    }
 
-  public function index() {
-    // /**layout manager**/
-    // $this->useLayout();
+    public function index(AuthInterface $auth, int $num = 42)
+    {
+        // /**layout manager**/
+        // $this->useLayout();
 
-    // $this->layout->setTitle("Ch'tis Gamers - Eilco");
-    // $this->layout->addStyle("materializeCG");
-    // $this->layout->addStyle("index/main");
-    // $this->layout->addStyle("components/hr");
-    // $this->layout->addScript("jquery-3.1.1");
-    // $this->layout->addScript("materialize");
-    // $this->layout->addScript("index/main");
+        // $this->layout->setTitle("Ch'tis Gamers - Eilco");
+        // $this->layout->addStyle("materializeCG");
+        // $this->layout->addStyle("index/main");
+        // $this->layout->addStyle("components/hr");
+        // $this->layout->addScript("jquery-3.1.1");
+        // $this->layout->addScript("materialize");
+        // $this->layout->addScript("index/main");
 
-    // /** get data form database **/
-    // $loader = new Loader();
-    // $loader->model('user');
-    // $loader->model('config');
-    // $loader->model('maincontent');
-    // $loader->model('sliders');
+        // /** get data form database **/
+        // $loader = new Loader();
+        // $loader->model('user');
+        // $loader->model('config');
+        // $loader->model('maincontent');
+        // $loader->model('sliders');
 
-    // $members = new User_Model();
+        // $members = new User_Model();
 
-    // $query = "SELECT * FROM users WHERE us_member=:member AND us_site=:site AND us_role!='Secrétaire' ORDER BY RAND()";
-    // $query = $members->execQuery($query, array('member' => 1, 'site' => "Calais"));
+        // $query = "SELECT * FROM users WHERE us_member=:member AND us_site=:site AND us_role!='Secrétaire' ORDER BY RAND()";
+        // $query = $members->execQuery($query, array('member' => 1, 'site' => "Calais"));
 
-    // $data["members"]["calais"] = $query->fetchall();
+        // $data["members"]["calais"] = $query->fetchall();
 
-    // $query = "SELECT * FROM users WHERE us_member=:member AND us_site=:site ORDER BY RAND()";
-    // $query = $members->execQuery($query, array('member' => 1, 'site' => "Longuenesse"));
+        // $query = "SELECT * FROM users WHERE us_member=:member AND us_site=:site ORDER BY RAND()";
+        // $query = $members->execQuery($query, array('member' => 1, 'site' => "Longuenesse"));
 
-    // $data["members"]["longuenesse"] = $query->fetchall();
+        // $data["members"]["longuenesse"] = $query->fetchall();
 
-    // $config_layout = (new Config_Model())->get("layout_id");
+        // $config_layout = (new Config_Model())->get("layout_id");
 
-    // $data["main"] = (new Maincontent_Model($config_layout))->get();
+        // $data["main"] = (new Maincontent_Model($config_layout))->get();
 
-    // $data["sliders"] = (new Sliders_Model())->get_multi();
+        // $data["sliders"] = (new Sliders_Model())->get_multi();
 
-    // $this->layout->render("index/index", $data);
+        // $this->layout->render("index/index", $data);
 
-    return ["reponce" => "test"];
-  }
+        return ["reponce" => $num, "auth" => $auth, "dic" => $this->injector->get(DefaultFirewall)];
+    }
 }
---
+++ System/Core/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Hexacore\Core;
+
+use Hexacore\Core\Request\RequestInterface;
+use Hexacore\Core\DI\DIC;
+use Hexacore\Core\Auth\AuthInterface;
+use Hexacore\Core\Storage\Session\SessionInterface;
+
+abstract class Controller
+{
+    protected $request;
+
+    protected $injector;
+
+    private $auth;
+
+    public function initialize(RequestInterface $request, DIC $dic, AuthInterface $auth)
+    {
+        $this->request = $request;
+        $this->injector = $dic;
+        $this->auth = $auth;
+    }
+
+    public function isGranted(StorageInterface $storage, string $role): bool
+    {
+        return $this->auth->isGranted($storage, $role);
+    }
+}
